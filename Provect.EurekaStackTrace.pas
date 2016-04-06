ee019dcccad7fde7b2903679f9710550befa18d5
Nick Remeslennikov <homolibere@pm.local>
2016-04-06 13:57:21 +0300

    eurekalog stacktrace added

---
+++ Provect.EurekaStackTrace.pas
@@ -0,0 +1,53 @@
+﻿unit Provect.EurekaStackTrace;
+
+interface
+
+uses
+  ECallStack, EDebugInfo,
+  System.Classes, System.SysUtils;
+
+function EuExceptionStackInfoProc(AExceptionRecord: PExceptionRecord): Pointer;
+function EuStackInfoStringProc(AInfo: Pointer): string;
+procedure EuCleanUpStackInfoProc(AInfo: Pointer);
+
+implementation
+
+function EuExceptionStackInfoProc(AExceptionRecord: PExceptionRecord): Pointer;
+var
+  Trace: String;
+  TraceSize: Integer;
+  EuBaseStackList : TEurekaStackListV7;
+begin
+  EuBaseStackList := nil;
+  try
+    EuBaseStackList := TEurekaStackListV7.Create(AExceptionRecord.ExceptionAddress);
+    Trace := EuBaseStackList.ToString;
+  finally
+    EuBaseStackList.Free;
+  end;
+  if not Trace.IsEmpty then
+  begin
+    TraceSize := (Length(Trace) + 1) * SizeOf(Char);
+    GetMem(Result, TraceSize);
+    Move(Pointer(Trace)^, Result^, TraceSize);
+  end
+  else
+    Result := nil;
+end;
+
+function EuStackInfoStringProc(AInfo: Pointer): string;
+begin
+  Result := PChar(AInfo);
+end;
+
+procedure EuCleanUpStackInfoProc(AInfo: Pointer);
+begin
+  FreeMem(AInfo);
+end;
+
+initialization
+  Exception.GetExceptionStackInfoProc := EuExceptionStackInfoProc;
+  Exception.GetStackInfoStringProc := EuStackInfoStringProc;
+  Exception.CleanUpStackInfoProc := EuCleanUpStackInfoProc;
+
+end.
--- README.md
+++ README.md
@@ -5,10 +5,21 @@ This repo is a collection of units that helps me in my development.
 # Description
 
 * Provect.Helpers.Str.pas
+
 Helpers for string class. Easy to use.
+
 * Provect.MemCache.pas
+
 Class for access to memcached. Supports all functions. Supported version up to 1.4.25
+
 * Provect.ModalController.pas
+
 An attempt to implement nice to use modal windows with cross platform support.
+
 * Provect.Pipes.pas
+
 Windows pipes as is.
+
+* Provect.EurekaStackTrace.pas
+
+Including this file in uses list you will get Exception.StackTrace filled
